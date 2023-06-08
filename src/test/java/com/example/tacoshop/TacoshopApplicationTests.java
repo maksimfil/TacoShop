a653d951f5d46e3362b6a3c4f8547b7f8ed0e83b
Fil_Maksim <filmaksim02@gmail.com>
2023-06-08 14:05:22 +0300

    First commit TacoShop.

---
+++ src/test/java/com/example/tacoshop/TacoshopApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.tacoshop;
+
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@SpringBootTest
+@RunWith(SpringRunner.class)
+class TacoshopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
